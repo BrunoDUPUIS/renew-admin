4748af81dac13776657120762dccafb1ebb6158c
Bruno <b-d.dupuis@laposte.net>
2026-06-10 20:27:12 +0200

    test

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import content from '../content/home.json'
+</script>
+
+<template>
+  <h1>{{ content.titre }}</h1>
+  <div>
+    <p>
+      {{ content.paragraph }}
+    </p>
+  </div>
+  <div>
+    <img :src="content.picture" alt="logo artisan" class="image" />
+  </div>
+</template>
+
+<style scoped>
+.image {
+  margin: 0 auto;
+  width: 500px;
+}
+h1 {
+  color: blueviolet;
+  text-align: center;
+  margin-top: 45px;
+}
+
+p {
+  text-align: center;
+}
+
+div {
+  margin: 0 auto;
+  border: 2px red solid;
+  width: 500px;
+}
+</style>
